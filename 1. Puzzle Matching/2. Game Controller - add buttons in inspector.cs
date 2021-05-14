0d783b34e6b522ebdb5e7d58e876be14dcd53446
yolabs007 <59382666+yolabs007@users.noreply.github.com>
2021-05-14 12:08:46 +0530

    Create 2. Game Controller - add buttons in inspector.cs

---
+++ 1. Puzzle Matching/2. Game Controller - add buttons in inspector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public List<Button> btns = new List<Button>();
+
+    void Start()
+    {
+        GetButtons();
+
+    }
+    void GetButtons()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleButton");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            btns.Add(objects[i].GetComponent<Button>());
+        }
+    }
+}
